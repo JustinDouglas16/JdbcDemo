875fe1c01d5ca300092630c21a99699daf3f58f0
JustinDouglas16 <justinrafael.douglas@gmail.com>
2023-11-06 20:36:10 -0300

    second commit

--- src/main/java/sr/unasat/jdbc/crud/app/Application.java
+++ src/main/java/sr/unasat/jdbc/crud/app/Application.java
@@ -1,7 +1,12 @@
 package sr.unasat.jdbc.crud.app;
 
+import sr.unasat.jdbc.crud.entities.Employee;
+import sr.unasat.jdbc.crud.repositories.EmployeeRepository;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Employee employee = new Employee(2, "Peter", "Parker", "peterparker@gmail.com");
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        employeeRepository.insertOneRecord(employee);
     }
 }
---
+++ src/main/java/sr/unasat/jdbc/crud/repositories/EmployeeRepository.java
@@ -0,0 +1,47 @@
+package sr.unasat.jdbc.crud.repositories;
+
+import org.jetbrains.annotations.NotNull;
+import sr.unasat.jdbc.crud.database.DatabaseConnectionManager;
+import sr.unasat.jdbc.crud.entities.Employee;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmployeeRepository {
+    private Connection connection;
+
+    public EmployeeRepository() {
+        this.connection = DatabaseConnectionManager.getConnection();
+    }
+
+    public void close() {
+        DatabaseConnectionManager.closeConnection(this.connection);
+    }
+
+    public int insertOneRecord(@NotNull Employee employee) {
+        PreparedStatement statement = null;
+        int result = 0;
+
+        try {
+            String sql = "INSERT INTO employee (id, first_name, last_name, email) VALUES (?, ?, ?, ?)";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, employee.getId());
+            statement.setString(2, employee.getFirstName());
+            statement.setString(3, employee.getLastName());
+            statement.setString(4, employee.getEmail());
+            result = statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+}
